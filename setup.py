3ef19be8d912a5f1ba0f86cb5c13fc432883c912
mgmateus <eng.mgmateus@gmail.com>
2024-02-08 09:16:42 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=[
+            'airsim_resources',
+            'open3d_resources'
+            ],
+    package_dir={"": "src/modules"})
+
+
+setup(**setup_args)
