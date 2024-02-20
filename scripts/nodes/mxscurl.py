5fbf554368e50c7cab6b8b1e56ae637c1d5cb906
mgmateus <eng.mgmateus@gmail.com>
2024-02-20 10:17:40 -0300

    store mesh

--- scripts/nodes/mxscurl.py
+++ scripts/nodes/mxscurl.py
@@ -8,15 +8,25 @@ import numpy as np
 
 
 from gym_airsim.environments import EnvironmentROS
+from gym_airsim.airsim_resources.airsim_base.types import Vector3r
 
 if __name__ == "__main__":
     rospy.init_node("mxscurl", anonymous=False)
     ip = os.environ['UE4_IP']
     env = EnvironmentROS(ip, 'Hydrone', 'stereo', 'rgb')
     
-    action = np.array([.3, .5, -1, .1, .1, .1])
-    env.vehicle.take_off()
-    for i in range(500):
-        pp = env.step(action)
-        rospy.logwarn(f"observation : {pp}")
-        rospy.Rate(10).sleep()
+    # meshs = env.vehicle.simGetMeshPositionVertexBuffers()
+    # rospy.logwarn(f"observation : {meshs[0]}")
+    v = Vector3r(401.95086669921875, -2541.569580078125, 1612.45849609375)
+    env.vehicle.simPlotPoints([v], size= 500, is_persistent = True)    
+    # action = np.array([.3, .5, -1, .1, .1, .1])
+    # env.vehicle.take_off()
+    # for i in range(500):
+    #     pp = env.step(action)
+        
+        
+    #     rospy.Rate(10).sleep()
+    
+    # 401.95086669921875,                                                              
+    #                 -2541.569580078125,                                                              
+    #                 1612.45849609375
