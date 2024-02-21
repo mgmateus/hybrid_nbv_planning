1ba0165b4dd248c4ff6a3a4f42fc5f2066a3c70a
mgmateus <eng.mgmateus@gmail.com>
2024-02-21 19:21:16 -0300

    mesh

--- scripts/nodes/mxscurl.py
+++ scripts/nodes/mxscurl.py
@@ -13,20 +13,16 @@ from gym_airsim.airsim_resources.airsim_base.types import Vector3r
 if __name__ == "__main__":
     rospy.init_node("mxscurl", anonymous=False)
     ip = os.environ['UE4_IP']
-    env = EnvironmentROS(ip, 'Hydrone', 'stereo', 'rgb')
+    env = EnvironmentROS(ip, 'Hydrone', 'stereo', 'rgb', '/home/airsim/AirSim/ros/src/hybrid_nbv_planning/files/vertices', 'semi_sub_8')
     
-    # meshs = env.vehicle.simGetMeshPositionVertexBuffers()
-    # rospy.logwarn(f"observation : {meshs[0]}")
-    v = Vector3r(401.95086669921875, -2541.569580078125, 1612.45849609375)
-    env.vehicle.simPlotPoints([v], size= 500, is_persistent = True)    
-    # action = np.array([.3, .5, -1, .1, .1, .1])
-    # env.vehicle.take_off()
+    
+    
+    action = np.array([.3, .5, -1, .1, .1, .1])
+    env.vehicle.take_off() 
+    time.sleep(5)
+    env.vehicle.test() 
     # for i in range(500):
     #     pp = env.step(action)
         
         
     #     rospy.Rate(10).sleep()
-    
-    # 401.95086669921875,                                                              
-    #                 -2541.569580078125,                                                              
-    #                 1612.45849609375
