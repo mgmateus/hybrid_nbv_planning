2118133e6cb40aa963808afbb39cf41e448f08d2
mgmateus <eng.mgmateus@gmail.com>
2024-02-23 08:33:45 -0300

    registration error cuda

---
+++ scripts/nodes/registration.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import rospy
+
+import numpy as np
+
+from open3d_resources.noetic import R3DROS
+
+if __name__ == "__main__":
+    rospy.init_node("registration", anonymous=False)
+    vehicle_name = "Hydrone"
+    depth_trunc = 100
+    width = 672
+    height = 376
+    fov = 90
+    rospy.logwarn('registration.py - main')
+    map = R3DROS(vehicle_name, depth_trunc, width, height, fov)
+    
+
+    rospy.spin()
